02da27afc27fb06d5676216ef6ef5ca4bbaa67b3
Dalhatmd <umarwalid4055@gmail.com>
2023-02-16 07:10:27 -0800

    Corrected 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	char stuff[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char stuff[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, stuff, 60);
+	write(2, stuff, 59);
 	return (1);
 }
