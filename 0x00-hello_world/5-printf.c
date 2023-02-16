02fc728365a192891c3ef56006cc0314a4221887
Dalhatmd <umarwalid4055@gmail.com>
2023-02-16 03:30:11 -0800

    Added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Execution starts here
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
