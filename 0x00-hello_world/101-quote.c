7dd77037c366b4333e053373d4add24d1dcb1c14
Dalhatmd <umarwalid4055@gmail.com>
2023-02-16 07:15:47 -0800

    Added quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,11 @@
  * main - program starts here
  *
  * Return: Always 1 (success)
- *
  */
 int main(void)
 {
-	char stuff[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, stuff, 59);
+	write(2, str1, 59);
 	return (1);
 }
