e0c4cd40b34c6fd065a0e0427282c3fca8788dd5
Dalhatmd <umarwalid4055@gmail.com>
2023-02-17 10:53:59 -0800

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all combinations of 0 - 9
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+
+	for (int x = 48; x < 58; x++)
+	{
+		putchar(x);
+		if (x != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar(10);
+	return (0);
+}
