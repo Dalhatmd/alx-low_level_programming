257af901809a74e6f9c6d847b01615b5e1e39a04
Dalhatmd <umarwalid4055@gmail.com>
2023-02-17 08:23:14 -0800

    Added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Execution starts here
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x = 48;
+
+	for (x = 48; x < 58; x++)
+		putchar(x);
+	putchar(10);
+	return (0);
+}
