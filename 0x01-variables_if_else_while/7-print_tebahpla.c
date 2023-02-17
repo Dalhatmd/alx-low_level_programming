3c1c9d48889f8613a01fa7e78ec7606fd17fe5b5
Dalhatmd <umarwalid4055@gmail.com>
2023-02-17 08:42:37 -0800

    Added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Execution starts
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 122; x > 96; x--)
+		putchar(x);
+	putchar(10);
+	return (0);
+}
