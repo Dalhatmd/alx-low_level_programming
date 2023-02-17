7a44e8f59dc641025e109900aa3a3b7e1f2fa6ea
Dalhatmd <umarwalid4055@gmail.com>
2023-02-17 08:07:45 -0800

    Added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Execution starts here
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+		printf("%d\n", x);
+	return (0);
+}
