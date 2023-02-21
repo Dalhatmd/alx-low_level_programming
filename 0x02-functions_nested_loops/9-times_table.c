489977501d0b5216c549504890c3786ca2a65feb
Dalhatmd <umarwalid4055@gmail.com>
2023-02-21 11:29:26 -0800

    Added 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main"
+/**
+ * times_table - Function that prints out the 9 times table
+ */
+void times_table(void)
+{
+	int a, b, mult;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			mult = a * b;
+			if (mult <= 9)
+				_putchar(mult + 48);
+			else
+			{
+				_putchar((mult / 10) + 48);
+				_putchar((mult % 10) + 48);
+			}
+			if (b >= 9)
+				continue;
+			_putchar(44);
+			_putchar(32);
+		}
+		_putchar(10);
+	}
+}
