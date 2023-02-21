002ae73ab79e380d4507b171146b16d171b60b6d
Dalhatmd <umarwalid4055@gmail.com>
2023-02-21 07:18:16 -0800

    Added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - functiom to print the absolute value of a number
+ *
+ * @int: input number
+ *
+ * Return: absolute value of int
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
