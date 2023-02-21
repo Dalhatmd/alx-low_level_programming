106648e4cdf1b370222d1427c3def70f82ec348e
Dalhatmd <umarwalid4055@gmail.com>
2023-02-21 08:02:50 -0800

    Added 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,5 +11,14 @@ int print_last_digit(int n)
 	int last;
 
 	last = n % 10;
-	return (last);
+	if (last < 0)
+	{
+		_putchar(-last + 48);
+		return (-last);
+	}
+	else
+	{
+		_putchar(last + 48);
+		return (last);
+	}
 }
