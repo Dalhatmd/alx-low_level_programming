25357a52a451d8fa1c0468b4e7018fe3fc58b521
Dalhatmd <umarwalid4055@gmail.com>
2023-02-21 00:16:44 -0800

    Added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints "_putchar" to the screen
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
