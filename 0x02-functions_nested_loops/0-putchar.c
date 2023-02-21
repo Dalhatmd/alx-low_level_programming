c285214e88486a89fc1733f980c6426fc1756cc3
Dalhatmd <umarwalid4055@gmail.com>
2023-02-21 05:05:14 -0800

    Added 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Program starts here
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch[] = {'_','p','u','t','c','h','a','r'};
+
+	int i;
+
+	for (i = 0 ;i < 9; i++)
+		_putchar(ch[i]);
+	return (0);
+}
