a4a08d6ebcefeb8fd506bea0f781c4dddec4a275
Dalhatmd <umarwalid4055@gmail.com>
2023-02-22 05:31:00 -0800

    Added 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Execution starts here
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+A directory for projects involving debugging
