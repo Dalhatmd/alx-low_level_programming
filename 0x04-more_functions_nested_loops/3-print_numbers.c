3bb48fabdb33dbefc492189e695d1450de2aefcb
Dalhatmd <umarwalid4055@gmail.com>
2023-02-23 09:15:24 -0800

    Added 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,12 @@
 #include "main.h"
+/**
+ * print_numbers - Prints from 0 - 9
+ */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i < 58; i++)
 	{
 		_putchar(i);
 	}
