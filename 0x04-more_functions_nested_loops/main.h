0d8d9e73cc3f7200aa62925dd2a97eb5ebc0acda
Dalhatmd <umarwalid4055@gmail.com>
2023-02-24 02:08:42 -0800

    corrections

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 int _isupper(int c);
 int _putchar(char c);
 int _isdigit(int c);
@@ -5,3 +6,4 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
