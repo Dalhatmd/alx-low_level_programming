1b56f3cc183cdb7a261749ae69f06799e6581a78
Dalhatmd <umarwalid4055@gmail.com>
2023-02-24 03:14:01 -0800

    Added 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * print_line - Prints an underscore by @n amount of times
+ *
+ * @n: number of times an underscore will be printed
+ */
 void print_line(int n)
 {
 	if (n <= 0)
 		putchar('\n');
 	else
-		putchar((95) * n);
+	{
+		int i;
+
+		for (int i = 0; i <= n; i++)
+			putchar(95);
+	}
 }
