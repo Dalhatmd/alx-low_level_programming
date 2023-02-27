e829cfcf412e9f855cf11df57200c666a5546a47
Dalhatmd <umarwalid4055@gmail.com>
2023-02-27 04:23:13 -0800

    Added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - counts the number of characters in a given string
+ *
+ * @s: string to be counted
+ *
+ * Return: Number of characters in @*s
+ */
+int _strlen(char *s)
+{
+	int i, count;
+
+	i = 0;
+	for (s[i]; s[i] != '\0'; i++)
+		count++;
+
+	return (count);
