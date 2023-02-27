bff7749987729670591484f426fc286795de1fcd
Dalhatmd <umarwalid4055@gmail.com>
2023-02-27 05:46:47 -0800

    Added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - Prints an input string in reverse
+ *
+ * @s: Input string
+ */
+void print_rev(char *s)
+{
+	int i, j, count;
+
+	char rev[];
+
+	count = 0;
+	while (str[count] != '\0')
+		count++;
+
+	j = count - 1;
+
+	for (i = 0; i < count; i++)
+	{
+		rev[i] = s[j];
+		j--;
+	}
+
+	for (j = 0; j < count + 1; j++)
+	{
+		putchar(rev[j]);
+	}
+}
