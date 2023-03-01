6abb9c24fca95f4477d2ddb6f9495c15ad630b89
Dalhatmd <umarwalid4055@gmail.com>
2023-03-01 21:03:18 +0100

    corrections

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,9 @@ char *_strncat(char *dest, char *src, int n)
 	j = 0;
 
 	while (dest[i] != '\0')
+	{
 		i++;
+	}
 
 	while (src[j] != '\0' && j < n)
 	{
@@ -26,5 +28,5 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	dest[i] = '\0';
 
-	return(dest);
+	return (dest);
 }
