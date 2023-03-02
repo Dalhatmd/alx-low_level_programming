b5f38b002e959822850ae34b2a7250a007e49288
Dalhatmd <umarwalid4055@gmail.com>
2023-03-02 06:32:46 +0100

    Added 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,19 +7,21 @@
  *
  * Return: Result of comparison, 0 if completely alike
  */
-char *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
+
 	int ret;
 
 	i = 0;
 	ret = 0;
 
-	while (ret = 0)
+	while (ret == 0)
 	{
 		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
-		ans = s1[i] - s2[i];
+		ret = s1[i] - s2[i];
 		i++;
 	}
-	return (ans);
+	return (ret);
+}
