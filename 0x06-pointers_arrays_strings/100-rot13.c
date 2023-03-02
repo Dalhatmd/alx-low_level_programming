d03cf6bba864f2424df5b8642c1bc85c5144bfbb
Dalhatmd <umarwalid4055@gmail.com>
2023-03-02 13:34:26 +0100

    Added rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - Encodes a string using rot13 encryption
+ *
+ * @s: String to be encoded
+ *
+ * Return: Encoded character
+ */
+char *rot13(char *s)
+{
+	char a[] = "abcdefghijklmABCDEFGHIJKLM";
+	char b[] = "nopqrstuvwxyzNOPQRSTUVWXYZ";
+
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0';j++)
+		{
+			if (s[i] == a[j])
+				s[i] = b[j];
+		}
+	}
+	return(s);
+}
