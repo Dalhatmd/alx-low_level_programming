a62fe0a5cab16c91f9b1c5bce18a93073f510331
Dalhatmd <umarwalid4055@gmail.com>
2023-03-06 15:38:30 +0100

    Added 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _stlrpbrk - Checks for the first occurence of a string
+ *
+ * @s: first input string
+ * @accept: second in put string
+ *
+ * Return: First occurence of character if found and null otherwise
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+				return (s + i);
+		}
+	}
+	return ('\0');
+}
