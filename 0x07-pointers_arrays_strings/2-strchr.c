994547f3ef7a7ef7ba562516b1a481997df537da
Dalhatmd <umarwalid4055@gmail.com>
2023-03-06 10:33:13 +0100

    Added 2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - searches a string for a character
+ *
+ * @s: string to be searched
+ * @c: character to be searched for
+ *
+ * Return: Characterbif it is found and null if it is not
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+			return (*(s + i));
+		return ('\0');
+	}
