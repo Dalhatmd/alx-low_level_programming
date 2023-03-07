f1da9f95776db832e0d39d2bf54cf0e4799f0a6a
Dalhatmd <umarwalid4055@gmail.com>
2023-03-07 23:24:39 +0100

    Added 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,10 +10,6 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	int count;
 
-	count = 0;
-	count++;
-	_strlen_recursion(s + 1);
-	return (count);
+	return (1 + _strlen_recursion(s + 1));
 }
