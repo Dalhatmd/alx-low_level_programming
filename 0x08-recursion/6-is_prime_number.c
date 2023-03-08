2733b97245ba2d48c5c23608808d05672a659a71
Dalhatmd <umarwalid4055@gmail.com>
2023-03-08 11:43:04 +0100

    Added 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,9 +10,9 @@
 int is_prime_helper(int n, int i)
 {
 	if (i == 1)
-		return 1;
+		return (1);
 	if (n % i == 0)
-		return 0;
+		return (0);
 	return is_prime_helper(n, i-1);
 }
 /**
@@ -25,6 +25,6 @@ int is_prime_helper(int n, int i)
 int is_prime_number(int n)
 {
 	if (n <= 1)
-		return 0;
-	return is_prime_helper(n, n-1);
+		return (0);
+	return is_prime_helper(n, n - 1);
 }
