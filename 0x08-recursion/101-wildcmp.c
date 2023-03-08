c72f8d4c346923bfec2cf21a42cb7d0ce33f0e78
Dalhatmd <umarwalid4055@gmail.com>
2023-03-08 14:47:32 +0100

    Added 101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * wildcmp - checks if 2 strings are identical while factoring s2 having the
+ * '*' special character
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 if identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2 || *s2 == '*')
+	{
+		if (*s2 == '*')
+		{
+			if (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)))
+				return (1);
+		}
+		return (wildcmp s1 + 1, s2 + 1);
+	}
+
+	return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,4 @@ int sqrt_helper(int n, int g);
 int is_prime_number(int n);
 int is_prime_helper(int n, int i);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
