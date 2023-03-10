498dec612c8c3b69c6f59be56c2e5453de427801
Dalhatmd <umarwalid4055@gmail.com>
2023-03-10 09:20:46 -0800

    fixed betty errors

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+/**
+ * main - prints every elsment of argv
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (success)
+ */
 int main(int argc, char **argv)
 {
 	int i;
 
-	for(i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 
