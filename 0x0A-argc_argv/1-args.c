24c3007845b0bbca330c83953cf1eb13c04b0e3e
Dalhatmd <umarwalid4055@gmail.com>
2023-03-10 16:32:22 +0100

    Made a correction

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,14 +9,6 @@
  */
 int main(int argc, char __attribute__((unused)) **argv)
 {
-	int i, count;
-
-	count = 0;
-	for (i = 1; i <= argc; i++)
-	{
-		count++;
-	}
-	printf("%d\n", count);
-	
-	return(0);
+	printf("%d\n", argc -1);
+	return (0);
 }
