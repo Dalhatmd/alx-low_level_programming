3ce8cc131e6fd549c5c156f9374d9f986684e0f6
Dalhatmd <umarwalid4055@gmail.com>
2023-03-13 15:10:42 +0100

    Made a correction to this

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,8 +3,8 @@
 /**
  * str_concat - concatenates two strings
  *
- * s1: first string
- * s2: second string
+ * @s1: first string
+ * @s2: second string
  *
  * Return: concatenated string
  */
@@ -13,14 +13,14 @@ char *str_concat(char *s1, char *s2)
 	int i, j, len;
 
 	char *a;
-	
+
 	len = 0;
 	j = 0;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 		len++;
