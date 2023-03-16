aec79b0716268c925eee99247c49ff6fc2b07ff4
dalhatmd <umarwalid4055@gmail.com>
2023-03-16 09:33:09 -0700

    Made a few fixes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 
-	for (i = 0; i < len1 && i < lens; i++)
+	for (i = 0; i < len1)
 		con[i] = s1[i];
 	for (j = 0; j < len2 && j < len2 && (i + j) < lens; j++)
 		con[i + j] = s2[j];
