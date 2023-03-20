4560f471122453583c04a1cacc1e132c447c6692
Dalhatmd <umarwalid4055@gmail.com>
2023-03-20 21:09:51 +0100

    Added documentation

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,10 @@
-#ifndef ABS
+#ifndef ABS_H
+#define ABS_H
+/*
+ * File: 3-function_like_macro.h
+ * Desc: computes the absolute value of a number
+ */
+
 
 #define ABS(X) ((X) > 0? (X) : (-1) * (X))
 
