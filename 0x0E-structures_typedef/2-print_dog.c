c6e031d36e46a578bda1e62fff6578ebc5d6a361
Dalhatmd <umarwalid4055@gmail.com>
2023-03-21 13:43:18 +0100

    fixed an error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,7 +4,7 @@
 /**
  * print_dog - function to print a struct
  *
- * @dog: struct to be printed
+ * @d: struct to be printed
  */
 void print_dog(struct dog *d)
 {
@@ -13,16 +13,16 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			printf("nil\n");
+			printf("Name: nil\n");
 
 		if (d->age > 0)
 			printf("Age: %f\n", d->age);
 		else
-			printf("nil");
+			printf("Age: nil\n");
 
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("nil");
+			printf("Owner: nil\n");
 	}
 }
