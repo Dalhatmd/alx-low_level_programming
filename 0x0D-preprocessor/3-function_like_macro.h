14bbb4880124c2dfaca5ebfbb510082f5dadb360
Dalhatmd <umarwalid4055@gmail.com>
2023-03-21 12:51:45 +0100

    fixed typo

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,10 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
 
 /*
  * File: 3-function_like_macro.h
  * Desc: computes the absolute value of a number
  */
-
-#define ABS(X) ((X) > 0? (X) : (-1) * (X))
+#define ABS(x) ((x) > 0? (x) : (-1) * (x))
 
 #endif
