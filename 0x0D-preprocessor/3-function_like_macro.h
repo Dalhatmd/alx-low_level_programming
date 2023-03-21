8a71bb281b95c9e6ea5c8df4f3a31d70039a2acd
Dalhatmd <umarwalid4055@gmail.com>
2023-03-21 13:03:29 +0100

    fixed betty errors

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -5,6 +5,6 @@
  * File: 3-function_like_macro.h
  * Desc: computes the absolute value of a number
  */
-#define ABS(x) ((x) > 0? (x) : (-1) * (x))
+#define ABS(x) ((x) > 0 ? (x) : (-1) * (x))
 
 #endif
