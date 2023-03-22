c15d37ed3b597152c80ddf634dbb6a9a13163bad
dalhatmd <umarwalid4055@gmail.com>
2023-03-22 13:09:19 -0700

    Added documentation

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,14 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+/**
+ * int_index - finds a character in an array
+ *
+ * @array: input array
+ * @size: size of array
+ * @cmp: pointer to function thaf compares characters
+ *
+ * Return: index of character if found and -1 on failure
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
