9d15aa79d2b938ff3e0a57720687c21669c8635c
Dalhatmd <umarwalid4055@gmail.com>
2023-03-22 16:35:03 +0100

    added a typedef for unsigned int

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,9 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
+/**
+ * typedef for unsigned int
+ */
+typedef unsigned int size_t;
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
