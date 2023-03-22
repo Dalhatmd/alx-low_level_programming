9d08d3f1506b58e5cadf143dd6160f653a38e1e6
Dalhatmd <umarwalid4055@gmail.com>
2023-03-22 16:43:54 +0100

    included a header for size_t

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,8 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
-/**
- * typedef for unsigned int
- */
-typedef unsigned int size_t;
+
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
