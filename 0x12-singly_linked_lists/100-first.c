447e0921df12ee3a00c4ede846882bfae2a42148
Dalhatmd <umarwalid4055@gmail.com>
2023-03-31 21:26:33 +0100

    Added 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,8 @@
 /**
  * my_constructor - prints a string before the main function runs
  */
-void my_constructor(void) __attribute__((constructor))
+void my_constructor(void) __attribute__ ((constructor));
+void my_constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
