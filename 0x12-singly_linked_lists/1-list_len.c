a494a1498c64691604a22191d427e90df29cb7f3
Dalhatmd <umarwalid4055@gmail.com>
2023-03-31 16:44:44 +0100

    Added 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+/**
+ * list_len - returns the counr of items in a linked list
+ *
+ * @h: list to be counted
+ *
+ * @Return: number of items in list
+ */
 size_t list_len(const list_t *h)
 {
 	size_t count;
@@ -7,7 +14,7 @@ size_t list_len(const list_t *h)
 	while (h != NULL)
 	{
 		count++;
-		h++;
+		h = h->next;
 	}
 	return (count);
 }
