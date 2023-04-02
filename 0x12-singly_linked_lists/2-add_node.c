260ce229f9f0ddb16bc064b0d74f8e7efe6431f6
dalhatmd <umarwalid4055@gmail.com>
2023-04-02 07:17:06 -0700

    Fixes for betty

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,7 @@
 #include <string.h>
 /**
  * add_node - adds a new node at the head of a linked list
- * 
+ *
  * @head: head of the linked list
  * @str: string to be copied to the new node
  *
@@ -19,7 +19,7 @@ list_t *add_node(list_t **head, const char *str)
 		len++;
 
 	new_node = malloc(sizeof(list_t));
-	if(new_node == NULL)
+	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
 	new_node->len = len;
