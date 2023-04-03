5aafd3afd399229b77ef75a24b83562027fde1ab
Dalhatmd <umarwalid4055@gmail.com>
2023-04-03 11:12:01 +0100

    Added documentation

--- 0x13-more_singly_linked_lists/1-list_len.c
+++ 0x13-more_singly_linked_lists/1-list_len.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+/**
+ * listint_len - returns the lenth of a linked list
+ *
+ * @h: list to be counted
+ *
+ * Return: number of elements in list
+ */
 size_t listint_len(const listint_t *h)
 {
 	size_t count;
