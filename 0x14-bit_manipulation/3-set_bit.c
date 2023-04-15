eb43384ebb3c6dd4e040727c5d06cc2cb771245d
dalhatmd <umarwalid4055@gmail.com>
2023-04-15 07:30:31 -0700

    fixed betty errors

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,7 +14,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	m = 1UL << index;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
-			return (-1);
+		return (-1);
 	*n = *n | m;
 
 	return (1);
