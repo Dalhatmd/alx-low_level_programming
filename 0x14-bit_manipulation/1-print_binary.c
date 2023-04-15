52e26df8c2add04c7c2f93b25c3c34425a8a0367
dalhatmd <umarwalid4055@gmail.com>
2023-04-15 14:55:45 +0100

    made a correction

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,26 +8,24 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int m;
-	int count, non_zero;
-
-	non_zero = 0;
+	int flag;
 
 	m = 1UL << (sizeof(unsigned long int) * 8 - 1);
-	count = 0;
+	flag = 0;
 
 	while (m > 0)
 	{
-		if (m <= n || non_zero || m == 1)
+		if ((n & m) == m)
 		{
-			putchar((n & m) ? '1' : '0');
-			non_zero = 1;
+			putchar('1');
+			flag = 1;
 		}
-		else
-			putchar(' ');
+		else if (flag == 1)
+		{
+			putchar ('0');
+		}
 		m >>= 1;
-		count++;
-		if ((count & 0x3) == 0)
-			putchar(' ');
 	}
-	putchar ('\n');
+	if (flag == 0)
+		putchar ('0');
 }
