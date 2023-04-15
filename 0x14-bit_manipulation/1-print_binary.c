d6a4e88f1e48d2f33f5c127c5445bbbce1cda74e
dalhatmd <umarwalid4055@gmail.com>
2023-04-15 09:37:33 +0100

    removed leading zeroes

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,14 +8,22 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int m;
-	int count;
+	int count, non_zero;
+
+	non_zero = 0;
 
 	m = 1UL << (sizeof(unsigned long int) * 8 - 1);
 	count = 0;
 
 	while (m > 0)
 	{
-		putchar((n & m) ? '1' : '0');
+		if (m <= n || non_zero || m == 1)
+		{
+			putchar((n & m) ? '1' : '0');
+			non_zero = 1;
+		}
+		else
+			putchar(' ');
 		m >>= 1;
 		count++;
 		if ((count & 0x3) == 0)
