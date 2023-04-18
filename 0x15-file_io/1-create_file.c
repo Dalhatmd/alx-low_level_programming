4bfaea5681a1279b5083cdc9c0fa4a1f0535fa59
dalhatmd <umarwalid4055@gmail.com>
2023-04-18 11:04:37 +0100

    Added documentation

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * create_file - creates a file
+ *
+ * @filename: name of file
+ * @text_content: content to be written to file
+ *
+ * Return: -1 if unsuccessful and 1 if successful
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int f;
