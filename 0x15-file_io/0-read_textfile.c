a7e25181ed6372ec8170a49d2ad1c355ca1f9e2f
Dalhatmd <umarwalid4055@gmail.com>
2023-05-08 18:33:44 +0100

    Made a few tweaks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,22 +1,39 @@
 #include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to STDERR
+ *
+ * @filename: name of file to be printed
+ * letters: number of characters to be printed
+ *
+ * Return: number of characters printed
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t n, nwrite;
-	int fd;
+	ssize_t n, nwrite, o;
 	char *buffer = NULL;
 
-	fd = open(filename, O_RDONLY);
+	o = open(filename, O_RDONLY);
 
-	if (fd == -1)
+	if (o == -1)
 		return (-1);
 
-	buffer = malloc(letters);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-	n = read(fd, buffer, letters);
+	n = read(o, buffer, letters);
 	nwrite = write(STDOUT_FILENO, buffer, n);
-	
+	if (nwrite == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	if (nwrite != n)
+	{
+		free(buffer);
+		return(0);
+	};
 	free(buffer);
-	close (fd);
-
+	close (o);
 	return (nwrite);
 }
