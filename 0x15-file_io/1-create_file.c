2dff918f19550706b8b2a6963e99666af0c9640f
Dalhatmd <umarwalid4055@gmail.com>
2023-05-08 19:22:27 +0100

    Added 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ *
+ * @filename: name of file
+ * @text_content: contenr to be written to file
+ *
+ * Return: 1 if successful and -1 on error
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w;
+	size_t len;
+	char *buffer;
+
+	len = 0;
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+	}
+	o = open(filename, O_TRUNC | O_RDWR | O_CREAT, 0600);
+	if (o == -1)
+		return (-1);
+	w = write(o, text_content, len);
+	if (w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
+
+
