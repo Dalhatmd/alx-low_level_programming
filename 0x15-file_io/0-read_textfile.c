372c29381169dc81d1772a90928115f66af8823b
Dalhatmd <umarwalid4055@gmail.com>
2023-05-10 01:35:37 +0100

    Small Tweak

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	o = open(filename, O_RDONLY);
 
 	if (o == -1)
-		return (-1);
+		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
