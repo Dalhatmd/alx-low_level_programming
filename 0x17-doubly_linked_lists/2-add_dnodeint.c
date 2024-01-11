7e0f5f31106819cf4d9d4c5d6d01840277dc0d58
Dalhatmd <umarwalid4055@gmail.com>
2024-01-11 13:14:12 +0100

    Betty fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,4 +1,12 @@
 #include "lists.h"
+/**
+ * add_dnodeint - adds a node at the head of a doubly linked list
+ *
+ * @head: head of the linked list
+ * @n: data to be added to the node
+ *
+ * Return: address of the new node
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = NULL;
