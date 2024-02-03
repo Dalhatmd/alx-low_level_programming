af6a61292838cdc8428c037171dc379a9bc53633
Dalhatmd <umarwalid4055@gmail.com>
2024-02-03 16:42:54 +0100

    fix task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -26,7 +26,7 @@ void hash_table_print(const hash_table_t *ht)
 			current = ht->array[i];
 			while (current)
 			{
-				printf("'%s: %s'", current->key, current->value);
+				printf("'%s': '%s'", current->key, current->value);
 				current = current->next;
 				if (current != NULL)
 					printf(", ");
