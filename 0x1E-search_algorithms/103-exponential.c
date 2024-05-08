6df8a4ae7e7b51d2328383460317773fcdbb8892
Dalhatmd <umarwalid4055@gmail.com>
2024-05-08 09:33:43 +0100

    Used exponentials of 2

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -25,7 +25,7 @@ int exponential_search(int *array, size_t size, int value)
 		printf("Value checked array[%d] = [%d]\n", j, array[j]);
 		if (array[j] == value)
 			return (j);
-		j = j * 2;
+		j = j << 1;
 	}
 
 	low = j / 2;
